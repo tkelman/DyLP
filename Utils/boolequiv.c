5054fe6229609d1a5c5f73a1b641ff322a460e2e
lou <lou@0333f2a5-a002-0410-9d66-cb58a90675f0>
2005-10-27 21:01:41 +0000

    Updated copyright notice. Added keyword property to makefiles.
    git-svn-id: https://projects.coin-or.org/svn/DyLP/Trunk@15 0333f2a5-a002-0410-9d66-cb58a90675f0

--- Utils/boolequiv.c
+++ Utils/boolequiv.c
@@ -1,10 +1,34 @@
+/*
+  This file is a part of the OsiDylp LP distribution.
+
+        Copyright (C) 2005 Lou Hafer
+
+        School of Computing Science
+        Simon Fraser University
+        Burnaby, B.C., V5A 1S6, Canada
+        lou@cs.sfu.ca
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St., Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 /*
   Rather than go through the bother required to get this string into the
   binary, we'll settle for a comment.
 
-  sccsid[] = "@(#)boolequiv.c	1.1	10/18/02" ;
-  svnid[] = "$Id" ;
+  sccs: sccsid[] = "@(#)boolequiv.c	1.1	10/18/02" ;
+  cvs/svn: svnid[] = "$Id$" ;
 */
 
 /*
