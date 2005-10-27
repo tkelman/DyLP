98f0ee397a9a740203ae6307192bbd2e775dd7a5
lou <lou@0333f2a5-a002-0410-9d66-cb58a90675f0>
2005-10-27 21:05:07 +0000

    Updated copyright notice. Added keywords property to makefiles.
    git-svn-id: https://projects.coin-or.org/svn/DyLP/Trunk@17 0333f2a5-a002-0410-9d66-cb58a90675f0

--- Dylp/consys_mathutils.c
+++ Dylp/consys_mathutils.c
@@ -1,7 +1,7 @@
 /*
   This file is a portion of the OsiDylp LP distribution.
 
-        Copyright (C) 2004 Lou Hafer
+        Copyright (C) 2005 Lou Hafer
 
         School of Computing Science
         Simon Fraser University
@@ -19,8 +19,8 @@
   more details.
 
   You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc., 59
-  Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St., Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /*
